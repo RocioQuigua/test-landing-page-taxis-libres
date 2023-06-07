ba5d9198566ee3429b147fc6f297b9f79e291837
Rocio Quigua <rocioquigualopez@gmail.com>
2023-06-06 22:39:39 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Presentation } from './components/organisms/Presentation/Presentation'
+import { SectionObjective } from './components/organisms/SectionObjective/SectionObjective'
+
+export const App = () => {
+  return (
+    <div>
+      <Presentation />
+      <SectionObjective />
+    </div>
+  )
+}
